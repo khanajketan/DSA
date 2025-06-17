a82e5fc98f4e1c808138a43e9c08cae371543b91
Ketan Khanaj <98208526+khanajketan@users.noreply.github.com>
2025-06-17 12:15:29 +0530

    Time: 2 ms (74.52%), Space: 53.4 MB (30.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,9 +3,9 @@ class Solution {
         int cad = 0;
         int cnt = 0;
 
-        for(int num: nums){
-            if(cnt == 0) cad = num;
-            if(num == cad)cnt++;
+        for(int num=0; num<nums.length;num++){
+            if(cnt == 0) cad = nums[num];
+            if(nums[num] == cad)cnt++;
             else cnt--;
         }
         return cad;
